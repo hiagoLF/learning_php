c542bda712cdc44e53794162d5459618d46567c7
hiagolf <hiagolof@gmail.com>
2022-01-08 18:44:02 -0300

    forms

---
+++ 5_forms/index.php
@@ -0,0 +1,32 @@
+<form method="POST" action="./receiver.php">
+    <h1>Login</h1>
+
+    <div>
+        <label for="login">
+            Nome
+        </label>
+        <input type="text" name="login">
+    </div>
+
+    <br />
+
+    <div>
+        <label for="email">
+            E-mail
+        </label>
+        <input type="email" name="email">
+    </div>
+
+    <br />
+
+    <div>
+        <label for="password">
+            Senha
+        </label>
+        <input type="password" name="password">
+    </div>
+
+    <br />
+
+    <button type="submit">Entrar</button>
+</form>
